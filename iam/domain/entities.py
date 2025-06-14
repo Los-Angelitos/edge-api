ec05e296e626c93ea0b6ed833d6aaa351cf0b4b3
Mauricio <mauriciorojasanchez@gmail.com>
2025-06-14 02:47:10 -0500

    refactor(device): updated device schema

--- iam/domain/entities.py
+++ iam/domain/entities.py
@@ -1,13 +1,13 @@
 ﻿class Device:
-    def __init__(self, id: int, ip_address: str, mac_address: str, state: str):
-        self.id = id
-        self.ip_address = ip_address
-        self.mac_address = mac_address
-        self.state = state
+    def __init__(self, device_id: str, api_key:str):
+        self.device_id = device_id
+        self.api_key = api_key
+
+    def to_dict(self):
+        return {
+            'device_id': self.device_id,
+            'api_key': self.api_key
+        }
 
     def __repr__(self):
         return f"Device(id={self.id}, ip_address='{self.ip_address}', mac_address='{self.mac_address}', state='{self.state}')"
-        
-    
-
-
